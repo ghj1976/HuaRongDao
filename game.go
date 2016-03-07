fe2a6d758dc1b785d32d7025d2141d766a215907
郭红俊 <guohongjun@weipass.cn>
2016-03-07 08:53:03 +0800

    使用图片铺满整个屏幕

---
+++ game.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"golang.org/x/mobile/asset"
+	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/exp/f32"
+	"golang.org/x/mobile/exp/sprite"
+	"golang.org/x/mobile/exp/sprite/clock"
+	"image"
+	"log"
+
+	_ "image/jpeg"
+)
+
+type Game struct {
+	lastCalc clock.Time // when we last calculated a frame
+}
+
+func (g *Game) InitScene(eng sprite.Engine, sz size.Event) *sprite.Node {
+
+	back := loadTextures(eng)
+
+	scene := &sprite.Node{}
+	eng.Register(scene)
+	eng.SetTransform(scene, f32.Affine{
+		{float32(sz.WidthPt), 0, 0},
+		{0, float32(sz.HeightPx), 0},
+	})
+
+	eng.SetSubTex(scene, back)
+
+	// newNode := func(fn arrangerFunc) {
+	// 	n := &sprite.Node{Arranger: arrangerFunc(fn)}
+	// 	eng.Register(n)
+	// 	scene.AppendChild(n)
+	// }
+
+	// newNode(func(eng sprite.Engine, n *sprite.Node, t clock.Time) {
+	// 	eng.SetSubTex(n, back)
+	// 	eng.SetTransform(n, f32.Affine{
+	// 		{128, 0, 0},
+	// 		{0, 128, 0},
+	// 	})
+	// })
+
+	return scene
+
+}
+
+// 加载纹理图
+func loadTextures(eng sprite.Engine) sprite.SubTex {
+	a, err := asset.Open("odd.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer a.Close()
+
+	m, _, err := image.Decode(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, err := eng.LoadTexture(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return sprite.SubTex{t, image.Rect(0, 0, 128, 128)}
+
+}
+
+func (g *Game) reset() {
+}
+
+func (g *Game) Press(down bool) {
+}
+
+func (g *Game) Update(now clock.Time) {
+}
+
+type arrangerFunc func(e sprite.Engine, n *sprite.Node, t clock.Time)
+
+func (a arrangerFunc) Arrange(e sprite.Engine, n *sprite.Node, t clock.Time) { a(e, n, t) }
