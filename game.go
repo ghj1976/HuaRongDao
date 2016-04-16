c73addb77afe150328cdaaecaeb14e9872cbfa68
郭红俊 <guohongjun@weipass.cn>
2016-04-16 09:28:24 +0800

    游戏区域绘制到图上

--- game.go
+++ game.go
@@ -14,11 +14,19 @@ import (
 	"io/ioutil"
 	"log"
 
-	_ "image/jpeg"
+	_ "image/png"
+)
+
+const (
+	GameAreaWidth    = 4.25 // 游戏区域宽度应该是小兵棋子的 4.25倍
+	ScreenAreaHeight = 6.5  // 屏幕区域高度，应该是小兵棋子的 6.5倍
+	GameAreaHeight   = 5.75 // 游戏区域高度，应该是小兵棋子的 6.5倍
 )
 
 var (
-	txtFont *truetype.Font // 游戏上显示文字时，用的字体，简单期间只用一个字体
+	txtFont              *truetype.Font // 游戏上显示文字时，用的字体，简单期间只用一个字体
+	ChessManWidth        float32        // 小兵棋子的宽度或者高度 ，单位 pt
+	GameAreaX, GameAreaY float32        // 游戏区域的左上角坐标，单位 pt
 )
 
 type Game struct {
@@ -27,6 +35,20 @@ type Game struct {
 }
 
 func (g *Game) InitScene(eng sprite.Engine, sz size.Event) *sprite.Node {
+
+	// 计算棋子兵应该的高度或长度。
+	ch := float32(sz.HeightPt) / ScreenAreaHeight
+	cw := float32(sz.WidthPt) / GameAreaWidth
+	if cw < ch {
+		ChessManWidth = cw
+		GameAreaX = 0.0
+		GameAreaY = float32(sz.HeightPt) - ChessManWidth*GameAreaHeight
+	} else {
+		ChessManWidth = ch
+		GameAreaX = (float32(sz.WidthPt) - ChessManWidth*GameAreaWidth) / 2
+		GameAreaY = float32(sz.HeightPt) - ChessManWidth*GameAreaHeight
+	}
+
 	scene := &sprite.Node{}
 
 	err := loadFont("./assets/f1.ttf")
@@ -35,8 +57,7 @@ func (g *Game) InitScene(eng sprite.Engine, sz size.Event) *sprite.Node {
 		return scene
 	}
 
-	// back := loadTextures(eng)
-	// b2 := loadTexturesB2(eng)
+	texs := loadTextures(eng)
 	b3 := loadFontTextTextures(eng, "哈哈", 46.0, color.Black, image.Rect(0, 0, 300, 150))
 
 	eng.Register(scene)
@@ -51,13 +72,13 @@ func (g *Game) InitScene(eng sprite.Engine, sz size.Event) *sprite.Node {
 		scene.AppendChild(n)
 	}
 
-	// newNode(func(eng sprite.Engine, n *sprite.Node, t clock.Time) {
-	// 	eng.SetSubTex(n, back)
-	// 	eng.SetTransform(n, f32.Affine{
-	// 		{float32(sz.WidthPt), 0, 0},
-	// 		{0, float32(sz.HeightPx), 0},
-	// 	})
-	// })
+	newNode(func(eng sprite.Engine, n *sprite.Node, t clock.Time) {
+		eng.SetSubTex(n, texs[texGameArea])
+		eng.SetTransform(n, f32.Affine{
+			{ChessManWidth * GameAreaWidth, 0, GameAreaX},
+			{0, ChessManWidth * GameAreaHeight, GameAreaY},
+		})
+	})
 
 	// newNode(func(eng sprite.Engine, n *sprite.Node, t clock.Time) {
 	// 	eng.SetSubTex(n, b2)
@@ -79,9 +100,73 @@ func (g *Game) InitScene(eng sprite.Engine, sz size.Event) *sprite.Node {
 
 }
 
-// 加载纹理图
-func loadTextures(eng sprite.Engine) sprite.SubTex {
-	a, err := asset.Open("odd.jpg")
+const (
+	texGameArea = iota // 游戏区域
+
+	texBtnReturn1 // 返回按钮 1
+	texBtnReturn2 // 返回按钮 2
+	texBtnReturn3 // 返回按钮 3
+	texBtnGuide1  // 攻略按钮 1
+	texBtnGuide2  // 攻略按钮 2
+	texBtnGuide3  // 攻略按钮 3
+	texBtnReload1 // 重玩按钮 1
+	texBtnReload2 // 重玩按钮 2
+	texBtnReload3 // 重玩按钮 3
+
+	texChessmanCaoCao1 // 棋子 曹操 1
+	texChessmanCaoCao2 // 棋子 曹操 2
+	texChessmanCaoCao3 // 棋子 曹操 3
+
+	texChessmanHGuanYu1     // 棋子 横 关羽 1
+	texChessmanHGuanYu2     // 棋子 横 关羽 2
+	texChessmanHGuanYu3     // 棋子 横 关羽 3
+	texChessmanHHuangZhong1 // 棋子 横 黄忠 1
+	texChessmanHHuangZhong2 // 棋子 横 黄忠 2
+	texChessmanHHuangZhong3 // 棋子 横 黄忠 3
+	texChessmanHMaChao1     // 棋子 横 马超 1
+	texChessmanHMaChao2     // 棋子 横 马超 2
+	texChessmanHMaChao3     // 棋子 横 马超 3
+	texChessmanHZhangFei1   // 棋子 横 张飞 1
+	texChessmanHZhangFei2   // 棋子 横 张飞 2
+	texChessmanHZhangFei3   // 棋子 横 张飞 3
+	texChessmanHZhaoYun1    // 棋子 横 赵云 1
+	texChessmanHZhaoYun2    // 棋子 横 赵云 2
+	texChessmanHZhaoYun3    // 棋子 横 赵云 3
+
+	texChessmanSGuanYu1     // 棋子 竖 关羽 1
+	texChessmanSGuanYu2     // 棋子 竖 关羽 2
+	texChessmanSGuanYu3     // 棋子 竖 关羽 3
+	texChessmanSHuangZhong1 // 棋子 竖 黄忠 1
+	texChessmanSHuangZhong2 // 棋子 竖 黄忠 2
+	texChessmanSHuangZhong3 // 棋子 竖 黄忠 3
+	texChessmanSMaChao1     // 棋子 竖 马超 1
+	texChessmanSMaChao2     // 棋子 竖 马超 2
+	texChessmanSMaChao3     // 棋子 竖 马超 3
+	texChessmanSZhangFei1   // 棋子 竖 张飞 1
+	texChessmanSZhangFei2   // 棋子 竖 张飞 2
+	texChessmanSZhangFei3   // 棋子 竖 张飞 3
+	texChessmanSZhaoYun1    // 棋子 竖 赵云 1
+	texChessmanSZhaoYun2    // 棋子 竖 赵云 2
+	texChessmanSZhaoYun3    // 棋子 竖 赵云 3
+
+	texChessmanA1 // 棋子 兵 甲 1
+	texChessmanA2 // 棋子 兵 甲 2
+	texChessmanA3 // 棋子 兵 甲 3
+	texChessmanB1 // 棋子 兵 乙 1
+	texChessmanB2 // 棋子 兵 乙 2
+	texChessmanB3 // 棋子 兵 乙 3
+	texChessmanC1 // 棋子 兵 丙 1
+	texChessmanC2 // 棋子 兵 丙 2
+	texChessmanC3 // 棋子 兵 丙 3
+	texChessmanD1 // 棋子 兵 丁 1
+	texChessmanD2 // 棋子 兵 丁 2
+	texChessmanD3 // 棋子 兵 丁 3
+
+)
+
+// 加载纹理图,多张纹理
+func loadTextures(eng sprite.Engine) []sprite.SubTex {
+	a, err := asset.Open("image.png")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -89,6 +174,7 @@ func loadTextures(eng sprite.Engine) sprite.SubTex {
 
 	m, _, err := image.Decode(a)
 	if err != nil {
+		log.Println("2")
 		log.Fatal(err)
 	}
 	t, err := eng.LoadTexture(m)
@@ -96,28 +182,9 @@ func loadTextures(eng sprite.Engine) sprite.SubTex {
 		log.Fatal(err)
 	}
 
-	return sprite.SubTex{t, image.Rect(0, 0, 128, 128)}
-
-}
-
-// 加载纹理图
-func loadTexturesB2(eng sprite.Engine) sprite.SubTex {
-	a, err := asset.Open("b2.jpeg")
-	if err != nil {
-		log.Fatal(err)
+	return []sprite.SubTex{
+		texGameArea: sprite.SubTex{t, image.Rect(0, 0, 1020, 1350)},
 	}
-	defer a.Close()
-
-	m, _, err := image.Decode(a)
-	if err != nil {
-		log.Fatal(err)
-	}
-	t, err := eng.LoadTexture(m)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return sprite.SubTex{t, image.Rect(0, 0, 267, 189)}
 
 }
 
